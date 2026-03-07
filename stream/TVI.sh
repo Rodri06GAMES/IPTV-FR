da7c7735c7a8cb1bd12478de99488a8fdd63f161
Rodri06GAMES <Gamingrodrigo2009@hotmail.com>
2026-03-07 15:41:44 +0000

    Update TVI.sh

--- stream/TVI.sh
+++ stream/TVI.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-CONTENT=$(wget -qO- "https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId= 2>/dev/null)" 2>/dev/null \
-| sed "s#edge_servers#https://video-auth6.iol.pt/edge_servers#g")
+TOKEN=$(wget -qO- "https://services.iol.pt/matrix?userId=" 2>/dev/null)
 
-if [ -z "$CONTENT" ]; then
-    echo "❌ Conteúdo m3u8 inválido."
+if [ -z "$TOKEN" ]; then
+    echo "❌ Token inválido."
     exit 1
 fi
 
-echo "$CONTENT" > TVI.m3u8
+echo "🔑 Token: $TOKEN"
+
+printf "#EXTM3U\nhttps://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=%s\n" "$TOKEN" > TVI.m3u8
 
 echo "✅ TVI.m3u8 criado com sucesso."
+cat TVI.m3u8
