5a44b029aded1289d139ac738c9c6af35653278d
Rodri06GAMES <Gamingrodrigo2009@hotmail.com>
2026-03-07 15:21:11 +0000

    Update TVI.sh

--- stream/TVI.sh
+++ stream/TVI.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "TVI.m3u8"
+TOKEN=$(wget -qO- "https://services.iol.pt/matrix?userId=" 2>/dev/null)
 
-exit 0
+if [ -z "$TOKEN" ]; then
+    echo "❌ Token inválido."
+    exit 1
+fi
+
+echo "🔑 Token obtido: $TOKEN"
+
+sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$TOKEN#g" stream/TVI.m3u8
+
+echo "✅ m3u8/TVI.m3u8 atualizado com sucesso."
