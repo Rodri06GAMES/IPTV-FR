296bacbbdcadc0445bfd857e63c8b6232e27578d
Rodri06GAMES <Gamingrodrigo2009@hotmail.com>
2026-03-07 15:32:42 +0000

    Update TVI.sh

--- stream/TVI.sh
+++ stream/TVI.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# 1️⃣ Obter o conteúdo completo do m3u8
 CONTENT=$(wget -qO- "https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId= 2>/dev/null)" 2>/dev/null \
 | sed "s#edge_servers#https://video-auth6.iol.pt/edge_servers#g")
 
@@ -9,12 +8,7 @@ if [ -z "$CONTENT" ]; then
     exit 1
 fi
 
-# 2️⃣ Criar o ficheiro com cabeçalho correto
-cat > stream/TVI.m3u8 << EOF
-#EXTM3U
-#EXT-X-VERSION:3
-$CONTENT
-EOF
+echo "$CONTENT" > stream/TVI.m3u8
 
-echo "✅ m3u8/TVI.m3u8 criado com sucesso."
+echo "✅ stream/TVI.m3u8 criado com sucesso."
 cat stream/TVI.m3u8
